11f0bda488d91448d6c8f27e778a1565b3bb292b
sushant <sushantsonawane121@gmail.com>
2022-12-09 18:38:52 +0530

    assignment pade added

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+
+// for redirecting on assignments page
+
+function redirect() {
+    // window.location.replace('../pages/assignments.html');
+    // window.location.reload('../pages/assignments.html');
+    window.location.href = '../pages/assignments.html';
+}
