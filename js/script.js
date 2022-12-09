78ebcfbe2daf5551cc854fcdee817aec407ca529
sushant <sushantsonawane121@gmail.com>
2022-12-09 18:46:13 +0530

    assignment btn fixed

--- js/script.js
+++
@@ -1,9 +0,0 @@
-
-
-// for redirecting on assignments page
-
-function redirect() {
-    // window.location.replace('../pages/assignments.html');
-    // window.location.reload('../pages/assignments.html');
-    window.location.href = '../pages/assignments.html';
-}
